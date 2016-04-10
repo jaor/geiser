s-abhinav/gitlab-geiser

commit 424553e017718c54e219212b27a32b341ec6bd28

Clean up indentation rules
Move general indentation rules to "geiser-syntax".

elisp/geiser-syntax.el
@@ -28,25 +28,30 @@
                     pairs)))
 
 (geiser-syntax--scheme-indent
+ (and-let* 1)
  (case-lambda 0)
  (catch defun)
  (class defun)
  (dynamic-wind 0)
  (let*-values 1)
- (letrec* 1)
- (letrec-values 1)
  (let-values 1)
  (let/ec 1)
- (match defun)
+ (letrec* 1)
+ (match 1)
+ (match-lambda 0)
+ (match-lambda* 0)
+ (match-let 1)
+ (match-let* 1)
+ (match-letrec 1)
  (opt-lambda 1)
  (parameterize 1)
  (parameterize* 1)
  (receive 2)
+ (require-extension 0)
  (syntax-case 2)
  (unless 1)
  (when 1)
  (while 1)
- (with-error-to-port 1)
  (with-syntax 1))
 
 

elisp/geiser-chicken.el
@@ -230,59 +230,35 @@ This function uses `geiser-chicken-init-file' if it exists."
    (geiser-syntax--simple-keywords geiser-chicken-builtin-keywords)))
 
 (geiser-syntax--scheme-indent
- (receive 2)
- (match 1)
- (match-lambda 0)
- (match-lambda* 0)
- (match-let scheme-let-indent)
- (match-let* 1)
- (match-letrec 1)
- (declare 0)
+ (assume 1)
+ (compiler-typecase 1)
  (cond-expand 0)
- (let-values scheme-let-indent)
- (let*-values scheme-let-indent)
- (letrec-values 1)
- (letrec* 1)
- (parameterize scheme-let-indent)
- (let-location 1)
+ (condition-case 1)
+ (cut 1)
+ (cute 1)
+ (declare 0)
+ (dotimes 1)
+ (ecase 1)
+ (fluid-let 1)
  (foreign-lambda 2)
  (foreign-lambda* 2)
  (foreign-primitive 2)
  (foreign-safe-lambda 2)
  (foreign-safe-lambda* 2)
- (set! 1)
- (let-optionals* 2)
- (let-optionals 2)
- (condition-case 1)
- (fluid-let 1)
- (and-let* 1)
- (assume 1)
- (cut 1)
- (cute 1)
- (when 1)
- (unless 1)
- (dotimes 1)
- (compiler-typecase 1)
- (ecase 1)
- (use 0)
- (require-extension 0)
- (import 0)
- (handle-exceptions 2)
- (regex-case 1)
- (define-inline 1)
- (define-constant 1)
- (define-syntax-rule 1)
- (define-record-type 1)
- (define-values 1)
- (define-record 1)
- (define-specialization 1)
- (define-type 1)
- (with-input-from-pipe 1)
- (with-output-to-pipe 1)
- (select 1)
  (functor 3)
- (define-interface 1)
- (module 2))
+ (handle-exceptions 2)
+ (import 0)
+ (let-location 1)
+ (let-optionals 2)
+ (let-optionals* 2)
+ (letrec-values 1)
+ (module 2)
+ (regex-case 1)
+ (select 1)
+ (set! 1)
+ (use 0)
+ (with-input-from-pipe 1)
+ (with-output-to-pipe 1))
 
 ;;; REPL startup
 

elisp/geiser-guile.el
@@ -281,14 +281,21 @@ This function uses `geiser-guile-init-file' if it exists."
 (geiser-syntax--scheme-indent
  (c-declare 0)
  (c-lambda 2)
+ (call-with-input-string 1)
+ (call-with-output-string 0)
  (lambda* 1)
  (pmatch defun)
  (sigaction 1)
+ (with-error-to-file 1)
+ (with-error-to-port 1)
+ (with-error-to-string 0)
  (with-fluid* 1)
  (with-fluids 1)
  (with-fluids* 1)
+ (with-input-from-string 1)
  (with-method 1)
- (with-mutex 1))
+ (with-mutex 1)
+ (with-output-to-string 0))
 
 
 

jaor/geiser

commit 5dda3732c142e7572783820509339674a9041f28

Fix indentation of named 'match-let'
This fixes a regression introduced by commit
424553e017718c54e219212b27a32b341ec6bd28.

elisp/geiser-syntax.el
@@ -41,7 +41,7 @@
  (match 1)
  (match-lambda 0)
  (match-lambda* 0)
- (match-let 1)
+ (match-let scheme-let-indent)
  (match-let* 1)
  (match-letrec 1)
  (opt-lambda 1)
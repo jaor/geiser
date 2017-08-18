f16817fc1b5a59c418b8a28353587d75b10deb19
Wojciech Gac <wojciech.s.gac@gmail.com>
2017-08-18 16:53:48 +0200

    Some rudimentary infrastructure for TinyScheme

---
+++ scheme/tiny/geiser/geiser.scm
@@ -0,0 +1,60 @@
+(define string-prefix?
+  (lambda (x y)
+    (let ((n (string-length x)))
+      (and (<= n (string-length y))
+           (let prefix? ((i 0))
+             (or (= i n)
+                 (and (char=? (string-ref x i) (string-ref y i))
+                      (prefix? (+ i 1)))))))))
+
+(define (split-by l p k)
+  (let loop ((low '())
+             (high '())
+             (l l))
+    (cond ((null? l)
+           (k low high))
+          ((p (car l))
+           (loop low (cons (car l) high) (cdr l)))
+          (else
+           (loop (cons (car l) low) high (cdr l))))))
+ 
+(define (sort f l)
+  (if (null? l)
+      '()
+      (split-by (cdr l) 
+                (lambda (x) (f (car l) x))
+                (lambda (low high)
+                  (append (sort f low)
+                          (list (car l))
+                          (sort f high))))))
+
+(define (filter pred lst)
+  (cond ((null? lst) '())
+        ((pred (car lst))
+         (cons (car lst) (filter pred (cdr lst))))
+        (else (filter pred (cdr lst)))))
+
+(define (call-with-output-string proc)
+  (let ((out (open-output-string)))
+    (proc out)
+    (get-output-string out)))
+
+(define (with-output-to-string thunk)
+  (call-with-output-string
+    (lambda (out)
+      (let ((old-out (current-output-port)))
+        (set-output-port out)
+        (thunk)
+        (set-output-port old-out)))))
+
+(define (write-to-string x)
+  (with-output-to-string
+    (lambda ()
+      (write x))))
+
+(define (geiser:completions prefix . rest)
+  rest
+  (sort string-ci<?
+        (filter (lambda (el)
+                  (string-prefix? prefix el))
+                (map write-to-string (apply append (oblist))))))
